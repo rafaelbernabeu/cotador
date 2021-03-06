4e100b9efb17f4d93cf1da14d12263e8eed2e7b5
Rafael Bernabeu <rbbernabeu@gmail.com>
2021-03-06 03:16:50 +0000

    cotacao service

--- src/main/java/service/CotacaoService.java
+++ src/main/java/service/CotacaoService.java
@@ -12,26 +12,30 @@ import java.util.stream.Stream;
 @ApplicationScoped
 public class CotacaoService {
 
-    public List<OpcaoDTO> geraCotacao(CotacaoDTO cotacao) {
+    public List<OpcaoDTO> geraCotacao(CotacaoDTO consulta) {
         Stream<Opcao> stream = Opcao.<Opcao>listAll().stream();
-        if (cotacao.getCategoria() != null && !cotacao.getCategoria().equals("")) {
-            stream = stream.filter(op -> op.getTabela().getCategoria().getNome().equals(cotacao.getCategoria()));
+        if (consulta.getCategoria() != null && !consulta.getCategoria().equals("")) {
+            stream = stream.filter(op -> op.getTabela().getCategoria().getNome().equals(consulta.getCategoria()));
         }
-        if (cotacao.getEstado() != null) {
-            stream = stream.filter(op -> op.getTabela().getEstado().getNome().equals(cotacao.getEstado().getNome()));
+        if (consulta.getEstado() != null) {
+            stream = stream.filter(op -> op.getTabela().getEstado().getNome().equals(consulta.getEstado().getNome()));
         }
-        if (cotacao.getProfissao() != null) {
-            stream = stream.filter(op -> op.getTabela().getEntidades().stream()
-                    .flatMap(e -> e.getProfissoes().stream())
-                    .anyMatch(p -> p.getNome().equals(cotacao.getProfissao().getNome())));
+        if (consulta.getProfissao() != null) {
+            stream = stream.filter(op -> op.getTabela().getEntidades().stream().flatMap(e -> e.getProfissoes().stream()).anyMatch(p -> p.getNome().equals(consulta.getProfissao().getNome())));
         }
-        if (cotacao.getCoparticipacao() != null) {
-            stream = stream.filter(op -> op.getCoparticipacao().equals(cotacao.getCoparticipacao()));
+        if (consulta.getCoparticipacao() != null) {
+            stream = stream.filter(op -> op.getCoparticipacao().equals(consulta.getCoparticipacao()));
         }
-        if (cotacao.getAcomodacao() != null && !cotacao.getAcomodacao().equals("")) {
-            stream = stream.filter(op -> op.getAcomodacao().getNome().equals(cotacao.getAcomodacao()));
+        if (consulta.getAcomodacao() != null && !consulta.getAcomodacao().equals("")) {
+            stream = stream.filter(op -> op.getAcomodacao().getNome().equals(consulta.getAcomodacao()));
         }
-        return stream.map(OpcaoDTO::new).collect(Collectors.toList());
+
+        List<OpcaoDTO> cotacao = stream.map(OpcaoDTO::new).collect(Collectors.toList());
+
+        if (consulta.getProfissao() != null) {
+            cotacao.forEach(c -> c.getTabela().getEntidades().removeIf(entidade -> !entidade.getProfissoes().contains(consulta.getProfissao())));
+        }
+        return cotacao;
     }
 
 }
