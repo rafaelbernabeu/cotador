e212589f46f1e6431040157e72b4825fba419b0b
Rafael Bernabeu <rbbernabeu@gmail.com>
2021-04-01 01:24:47 +0000

    melhorando script de deploy

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+rm src/main/resources/META-INF/resources/favicon.ico
+rm src/main/resources/META-INF/resources/index.html
+rm src/main/resources/META-INF/resources/main.*.js
+rm src/main/resources/META-INF/resources/runtime.*.js
+rm src/main/resources/META-INF/resources/polyfills.*.js
+rm src/main/resources/META-INF/resources/styles.*.css
+rm src/main/resources/META-INF/resources/3rdpartylicenses.txt
+
+cd ../cotador-app
+ng build --prod
+
+cp dist/cotador-app/* ../cotador/src/main/resources/META-INF/resources
+cd -
+
 ./build-native.sh && ./deploy-heroku.sh
